d36e7a4c5aac3bcd257fac105dfec1af54137e40
NaimAftab <naima.aftab@reseau.sncf.fr>
2021-11-30 18:50:16 +0100

    creation of my footer --done

---
+++ monblog/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classNames from '../styles/components/footer.module.css';
+
+const Footer = () => {
+return (
+    <div className = {classNames.monFooter}>
+      <ul className = {classNames.monUlFooter}>
+        <li>&copy; Naima Aftab - Tous droits réservés.</li>
+        <li><a href="https://example.com">Politique de confidentialité</a></li>
+        <li><a href="https://example.com">Mentions légales</a></li>
+        <li><a href="https://example.com">A propos</a></li>
+      </ul>
+    </div>)
+}
+
+export default Footer;
--- monblog/src/pages/Home.jsx
+++ monblog/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Footer />
     </div>
   );
 };
